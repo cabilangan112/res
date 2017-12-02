c9b202377a35e0cc54dfed9664b5210fcd826e6d
cabilangan112 <jassencabilangan@gmail.com>
2017-12-02 13:49:24 +0800

    before templates

--- myrecommendations/urls.py
+++ myrecommendations/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^Restaurant/', include('restaurants.urls', namespace='restaurants')),
-]
+	url(r'^$', RedirectView.as_view(url='/restaurants/', permanent=True)),
+	url(r'^accounts/',include('django.contrib.auth.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
